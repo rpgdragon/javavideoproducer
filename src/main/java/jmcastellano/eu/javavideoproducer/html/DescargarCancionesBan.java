a24f0722c67e40489d11aa450d3f8ad7c30bcf0b
Jose Manuel Castellano <jmcastellano@jmcastellano.eu>
2022-03-17 22:49:31 +0100

    Añadido fichero canciones

---
+++ src/main/java/jmcastellano/eu/javavideoproducer/html/DescargarCancionesBan.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmcastellano.eu.javavideoproducer.html;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import jmcastellano.eu.javavideoproducer.modelo.Constantes;
+
+/**
+ *
+ * @author rpgdragon
+ */
+public class DescargarMensajes {
+    
+    private ArrayList<String> mensajes;
+    private boolean enEjecucion = false;
+    
+    public DescargarMensajes(){
+        
+    }
+    
+    public void obtenerMensajes(){
+        if(mensajes==null || mensajes.isEmpty()){
+            enEjecucion = true;
+            mensajes = new ArrayList<>();
+            Thread t = new Thread(() -> {
+                try {
+                    URL url = new URL(Constantes.URL_MENSAJES);
+                    Scanner s = new Scanner(url.openStream(),"UTF-8");
+                    while(s.hasNextLine()){
+                        String cad = s.nextLine();
+                        mensajes.add(cad);
+                    }
+                } catch(Exception e){}
+                finally{
+                    enEjecucion = false;
+                }
+            });
+            t.start();
+        }
+    }
+
+    public ArrayList<String> getMensajes() {
+        return mensajes;
+    }
+
+    public boolean isEnEjecucion() {
+        return enEjecucion;
+    }
+    
+    public String dameMensajeAleatorio(){
+        if(mensajes!=null && !mensajes.isEmpty()){
+            Random r = new Random();
+            int tam = mensajes.size();
+            
+           return mensajes.get(r.nextInt(tam));
+        }
+        return "";
+    }
+}
